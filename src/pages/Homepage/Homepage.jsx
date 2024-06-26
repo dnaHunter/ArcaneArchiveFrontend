c9b3e6fde7f95cd2d48dd5370f47c2e1667ecdfd
Daniel Aston <daniel.aston@live.co.uk>
2024-06-26 16:24:53 +0100

    Add api call to /books

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,35 +1,49 @@
+import { useEffect, useState } from "react";
 import BookIcon from "../../components/BookIcon/BookIcon";
 import "./Homepage.scss";
+import axios from "axios";
 
 export default function Homepage() {
+  const [bookList, setBookList] = useState(null);
+  const [error, setError] = useState(false);
+
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+  async function getBookList() {
+    try {
+      const { data } = await axios.get(`${BACKEND_URL}/books`);
+      console.log(`${BACKEND_URL}/books`);
+      
+      setBookList(data);
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    getBookList();
+  }, []);
+
+  if (error) {
+    console.log(error);
+    return <p>{error.messsage}</p>;
+  }
+
+  if (!bookList) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section className="home">
       <section className="home__all">
-      <BookIcon
-        URL={"https://picsum.photos/200/300"}
-        title={"Test"}
-        author={"Test"}
-      />
-      <BookIcon
-        URL={"https://picsum.photos/200/300"}
-        title={"Test"}
-        author={"Test"}
-      />
-      <BookIcon
-        URL={"https://picsum.photos/200/300"}
-        title={"Test"}
-        author={"Test"}
-      />
-      <BookIcon
-        URL={"https://picsum.photos/200/300"}
-        title={"Test"}
-        author={"Test"}
-      />
-      <BookIcon
-        URL={"https://picsum.photos/200/300"}
-        title={"Test"}
-        author={"Test"}
-      />
+        {bookList.map((book) => (
+          <BookIcon
+            key={book.id}
+            URL={"https://picsum.photos/200/300"}
+            title={book.title}
+            author={book.author}
+          />
+        ))}
       </section>
     </section>
   );
